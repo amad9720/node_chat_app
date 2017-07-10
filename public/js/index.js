9d6f83a516db41ee0e9fe745e7e1cd2af8984b3b
Amadou Ly <amly212782@gmail.com>
2017-07-10 18:48:29 +0200

    Adding notifications

--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,20 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to the server !');
-
-    socket.emit('createMessage', {
-        from : "juliaElen@gmail.com",
-        body: "Hey nice to meet you too Amadou ",
-    });
-
 });
 
 socket.on('disconnect', function () {
-    console.log('Disconnected from to the server !');
+    //console.log('Disconnected from to the server !');
 });
 
 socket.on('newMessage', function (Data) {
    console.log('New Message  :', Data); 
+});
+
+socket.on('login', function(data) {
+    console.log("Admin message", data);
+});
+
+socket.on('userJoined', function(data) {
+    console.log("Admin message", data);
 });
--- server/server.js
+++ server/server.js
@@ -12,11 +12,23 @@ app.use(express.static(path.join(__dirname, '../public')));
 var server = http.createServer(app);
 var io = socketIO(server);
 
+var date = new Date();
+
 io.on('connection', (socket) => {
-    console.log('New USER connected !');
+    //console.log('New USER connected !');
+    socket.emit("login", {
+        from : "Admin",
+        body: "You are connected"
+    });
+
+    socket.broadcast.emit('userJoined', {
+        from : "Admin",
+        body: "New user Joined the Chat",
+        createdAt: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    });
 
     socket.on('disconnect', (socket) => {
-        console.log('USER disconnected !');
+        //console.log('USER disconnected !');
     });
 
     socket.on('createMessage', function (Data) {
@@ -24,20 +36,27 @@ io.on('connection', (socket) => {
         
         //io.emit emit an event to every single connection
         //this will take the message emited from a single user and share it with all the users
-        io.emit('newMessage',{
-            from: Data.from,
-            body: Data.body,
-            createdAt: new Date().getDate()
-        });
+            io.emit('newMessage',{
+                from: Data.from,
+                body: Data.body,
+                createdAt: new Date().getDate()
+            });
 
+        //Now we want to emit to all users but the one who send the message... for that we will use broadcasting
+        //Broadcasting is like io but it will execpt the user who is sending
+            // socket.broadcast.emit('newMessage',{
+            //     from: Data.from,
+            //     body: Data.body,
+            //     createdAt: new Date().getDate()
+            // });
     })
 
     //socket.emit emit an event to a single connection
-    socket.emit('newMessage', {
-        from : "amly@gmail.com",
-        body: "Hey nice to meat you Elen",
-        createdAt: new Date().getDate()
-    });
+    // socket.emit('newMessage', {
+    //     from : "amly@gmail.com",
+    //     body: "Hey nice to meat you Elen",
+    //     createdAt: new Date().getDate()
+    // });
 });
 
 server.listen(port, () => {
